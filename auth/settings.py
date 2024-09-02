5554aaa30bfa7a246491dc99fad8b836f17572a3
surajmendhe5573 <surajmendhe470@gmail.com>
2024-09-02 19:48:59 +0530

    connnect with postgres

--- auth/settings.py
+++ auth/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "dj auth",
+        "USER": "postgres",
+        "PASSWORD": "suraj",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
